635b86614b4bfe927b9a8d10d04be340d1fcbb35
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-10-12 16:54:52 +0530

    Initial commit

---
+++ app/src/main/java/me/mtron/mobile_application_development_labsheet6/example_01_adding_fragments_statistically/Activity1.java
@@ -0,0 +1,16 @@
+package me.mtron.mobile_application_development_labsheet6.example_01_adding_fragments_statistically;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.mtron.mobile_application_development_labsheet6.R;
+
+public class Activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1);
+    }
+}
---
+++ app/src/main/java/me/mtron/mobile_application_development_labsheet6/example_01_adding_fragments_statistically/Fragment2.java
@@ -0,0 +1,26 @@
+package me.mtron.mobile_application_development_labsheet6.example_01_adding_fragments_statistically;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import me.mtron.mobile_application_development_labsheet6.R;
+
+public class Fragment2 extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_2, container, false);
+    }
+}
